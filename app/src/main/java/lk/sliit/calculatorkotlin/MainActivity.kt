b95da223e43b57fb6142638e86a6545ee735f9f3
Dilanka Seneviratne <dvseneviratne@gmail.com>
2023-05-04 15:17:55 +0530

    UI complete, backend started
    Finished the general structure of the GUI through the xml file. Started on the main kotlin file to add the backend functions

--- app/src/main/java/lk/sliit/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/lk/sliit/calculatorkotlin/MainActivity.kt
@@ -2,10 +2,24 @@ package lk.sliit.calculatorkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_clear.setOnClickListener {
+            input.text = ""
+            output.text = ""
+        }
+        button_bracket_left.setOnClickListener {
+            input.text = ""
+            output.text = ""
+        }
+    }
+
+    private fun addToInputText(buttonValue : String ): String{
+        
     }
 }
